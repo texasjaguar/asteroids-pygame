80f449b43cbe7911305488bead89a68ccd9d13b1
texasjaguar <nikitagb07@gmail.com>
2025-12-14 23:29:48 +0300

    1: added logs

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print("Screen width: 1280\nScreen height: 720")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
